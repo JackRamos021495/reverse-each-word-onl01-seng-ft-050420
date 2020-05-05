ead4edadcd1ee3cf85c5ee4ecd19f76e9dc29ae7
Jackson Ramos <jackramos021495@gmail.com>
2020-05-05 22:01:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ def reverse_each_word(sentence)
   #sentence.split
  array = []
  array << sentence
+ new_array = []
   array.collect do |word|
-    word.reverse
+    new_array << word.reverse
   end
-  array.join(" ")
 end
